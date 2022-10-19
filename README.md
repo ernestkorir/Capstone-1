b5783d4566b5fb734e02e8b407d5cdc9c9d09b6a
ernest korir <ernestbore3@gmail.com>
2022-10-19 13:20:33 +0300

    Add Speakers Section dynamically

--- README.md
+++ README.md
@@ -10,14 +10,14 @@
 - Javascript
 - Technologies used
 
-## Live Demo (if available)
+## Live Demo
 
-[Live Demo Link](https://livedemo.com)
+[Live Demo](https://ernestkorir.github.io/Capstone-1/)
+
+## Loom Video
+[Loom Video]()
 
 ## Getting Started
-
-**This is an example of how you may give instructions on setting up your project locally.**
-**Modify this file to match your project, remove sections that don't apply. For example: delete the testing section if the currect project doesn't require testing.**
 
 To get a local copy up and running follow these simple example steps.
 
@@ -25,14 +25,6 @@ To get a local copy up and running follow these simple example steps.
 
 You can use the provided link to clone it:
 https://github.com/ernestkorir/Capstone-1.git
-
-### Install
-
-### Usage
-
-### Run tests
-
-### Deployment
 
 ## Authors
 
@@ -54,10 +46,11 @@ Give a ⭐️ if you like this project!
 
 ## Acknowledgments
 
+- To [Cindy Shin](https://www.behance.net/gallery/29845175/CC-Global-Summit-2015)
 - Hat tip to anyone whose code was used
 - Inspiration
 - etc
 
 ## 📝 License
 
-This project is [MIT](./LICENSE) licensed.
+This project is [MIT](./MIT.md) licensed.
---
+++ script.js
@@ -0,0 +1,118 @@
+function toggleMenu() {
+  document.querySelector('#burger-container').classList.toggle('invisible');
+  document.querySelector('#nav').classList.toggle('invisible');
+}
+
+document.getElementById('burger-container').onclick = function () { toggleMenu(); };
+
+const speakers = [
+  {
+    imagesURL: ['./assets/11-chess-board-removebg-preview.png', './assets/11-chess-board-gray.png', './assets/person-1.jpg'],
+    name: 'June Jusper',
+    occupation: 'CEO of Golden View School',
+    info: 'June is the CEO of Golden View School, She is a founded this school for the aim of supporting students to acquire Knowleadge and skills they need',
+  },
+  {
+    imagesURL: ['./assets/11-chess-board-removebg-preview.png', './assets/11-chess-board-gray.png', './assets/person-2.jpg'],
+    name: 'Anite Caren',
+    occupation: 'Managing Director',
+    info: 'She ensures all students get the materials they need for they study and ensure everything else run smooth for both online classes and physical',
+
+  },
+  {
+    imagesURL: ['./assets/11-chess-board-removebg-preview.png', './assets/11-chess-board-gray.png', './assets/persons2.jpg'],
+    name: 'Faith Mercy',
+    occupation: 'Admission Officer',
+    info: 'Mercy will be the one in-charge of your admission process and admits you to the system and provide neccesary assistant when required.You will enjoy the admission process with her',
+  },
+  {
+    imagesURL: ['./assets/11-chess-board-removebg-preview.png', './assets/11-chess-board-gray.png', './assets/person-4.jpg'],
+    name: 'Brian Kevin',
+    occupation: 'Career Coach',
+    info: 'Brian will be in-charge and will be a person to talk to frequently about your career and will advise alot about the career path you want to take',
+  },
+  {
+    imagesURL: ['./assets/11-chess-board-removebg-preview.png', './assets/11-chess-board-gray.png', './assets/persons1.jpg'],
+    name: 'Andrés Couttenye',
+    occupation: 'German Teacher at Obelisk Language Center',
+    info: 'Andrés will be speaking about the rising demand of professionals that it\'s taking place in Germany recently. How learning German could help you land a job will be discussed in this forum',
+  },
+  {
+    imagesURL: ['./assets/11-chess-board-removebg-preview.png', './assets/11-chess-board-gray.png', './assets/person-6.jpg'],
+    name: 'Hart Davies',
+    occupation: 'Dean of Students',
+    info: 'Hart is the dean of students and ensure the students experience a nice environment in school.He also ensure the school rules and regulation are followed to ensure smooth running',
+  },
+];
+
+function htmlGen(num) {
+  for (let i = 0; i < num; i += 1) {
+    const spkCard = `
+      <div class="speaker-card">
+        <img src="#" alt="" class="speaker-deco">
+        <img src="#" alt="Headshot picture" class="speaker-img">
+        <div class="speaker-info">
+          <h3 class="speaker-name"></h3>
+          <h4 class="speaker-occ" ></h4>
+          <hr>
+          <p class="speaker-para"></p>
+        </div>
+      </div>
+    `;
+
+    document.getElementById('speaker-container').innerHTML += spkCard;
+
+    const deco = document.querySelectorAll('.speaker-deco');
+    const img = document.querySelectorAll('.speaker-img');
+    const name = document.querySelectorAll('.speaker-name');
+    const occ = document.querySelectorAll('.speaker-occ');
+    const info = document.querySelectorAll('.speaker-para');
+
+    if (window.innerWidth > 768) {
+      deco[i].setAttribute('src', `${speakers[i].imagesURL[1]}`);
+    } else {
+      deco[i].setAttribute('src', `${speakers[i].imagesURL[0]}`);
+    }
+
+    img[i].setAttribute('src', `${speakers[i].imagesURL[2]}`);
+    name[i].innerHTML = speakers[i].name;
+    occ[i].innerHTML = speakers[i].occupation;
+    info[i].innerHTML = speakers[i].info;
+  }
+}
+
+function spkBtnValid() {
+  if (document.querySelector('#morles').innerHTML === 'MORE ') {
+    return false;
+  }
+  return true;
+}
+
+function widthTeller() {
+  if (spkBtnValid() || window.innerWidth > 768) {
+    document.getElementById('speaker-container').innerHTML = '';
+    htmlGen(6);
+  } else {
+    document.getElementById('speaker-container').innerHTML = '';
+    htmlGen(2);
+  }
+  return window.innerWidth;
+}
+
+function toggleSpeakers() {
+  const btnValue = document.getElementById('morles').innerHTML;
+  if (btnValue === 'MORE ') {
+    document.getElementById('morles').innerHTML = 'LESS ';
+    document.getElementById('chevron').classList.replace('fa-chevron-down', 'fa-chevron-up');
+    return widthTeller();
+  }
+  document.getElementById('morles').innerHTML = 'MORE ';
+  document.getElementById('chevron').classList.replace('fa-chevron-up', 'fa-chevron-down');
+  return widthTeller();
+}
+
+const btn = document.querySelector('.speaker-btn');
+window.addEventListener('resize', widthTeller);
+btn.onclick = function () { toggleSpeakers(); };
+
+widthTeller();
